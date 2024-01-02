3098a8129a32787c3fd3bfd164eaef724814c962
Savin Miruna <miruna.savin02@gmail.com>
2024-01-02 20:30:42 +0200

    Complete crawler(add neighbours)

--- Proiect/main.py
+++ Proiect/main.py
@@ -5,7 +5,10 @@ import requests
 from bs4 import BeautifulSoup
 import psycopg2
 
+from common import connect_to_database
+
 indexPageURL = 'https://en.wikipedia.org/wiki/List_of_countries_and_dependencies_by_population'
+neighboursPageURL = 'https://en.wikipedia.org/wiki/List_of_countries_and_territories_by_number_of_land_borders'
 
 
 class State:
@@ -41,8 +44,8 @@ class State:
         print(f'State:{self.name},'
               f' Capital : {self.capital},'
               f' Population : {self.population},'
-              f' Population density : {self.population_density},'
-              f' Total area: {self.area},'
+              f' Population density : {self.population_density} / square km,'
+              f' Total area: {self.area} square km,'
               f' Language : {self.language},'
               f' Time zone: {self.time_zone},'
               f' Political regime: {self.political_regime}')
@@ -87,9 +90,56 @@ def fetch_states(conn):
         conn.close()
 
 
+def fetch_state_neighbours(conn):
+    url = neighboursPageURL
+    response = requests.get(url)
+
+    cursor = conn.cursor()
+
+    if response.status_code != 200:
+        raise Exception(f"Cannot access wikipedia list of countries; status code : {response.status_code}")
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+    table = soup.find('table', {'class': 'wikitable'})
+
+    states_and_neighbours = {}
+    for row in table.find_all('tr')[2:]:
+        columns = row.find_all('td')
+        state_letters = takewhile(lambda x: not x.isdigit() and not is_character_parantheses(x) and x != ',',
+                                    columns[0].text.strip().lower())
+        state_name = ''.join(state_letters)
+        neighbours_data = columns[5].find_all('a')
+
+        neighbours = []
+
+        for a_tag in neighbours_data:
+            text = a_tag.get_text(strip=True)
+            if '[' not in text and ']' not in text:
+                neighbours.append(text)
+
+        neighbours_string = ', '.join(neighbours)
+        states_and_neighbours[state_name] = neighbours_string
+
+    try:
+        for state, neighbours in states_and_neighbours.items():
+            update_query = "UPDATE states_of_the_world SET neighbours = %s WHERE LOWER(name) = %s"
+            cursor.execute(update_query, (neighbours, state))
+        conn.commit()
+
+    except psycopg2.Error as e:
+        conn.rollback()
+        print('Database error:', e)
+    finally:
+        cursor.close()
+        conn.close()
+
+    return states_and_neighbours
+
 def is_character_part_of_number(x):
     return x.isdigit() or x == ',' or x == '.'
 
+def is_character_parantheses(x):
+    return x == '(' or x == ')' or x == ']' or x == '[' or x == '}' or x == '{'
 
 def fetch_state_info(state_name, state_url):
     response = requests.get(state_url)
@@ -106,7 +156,7 @@ def fetch_state_info(state_name, state_url):
                 if header:
                     header_text = header.get_text(strip=True)
                     if 'capital' in header_text.lower():
-                        capital_letters = takewhile(lambda x: not x.isdigit(), row.find('td').get_text(strip=True))
+                        capital_letters = takewhile(lambda x: not x.isdigit() and not is_character_parantheses(x), row.find('td').get_text(strip=True))
                         country_info['Capital'] = ''.join(capital_letters)
                         continue
 
@@ -133,7 +183,7 @@ def fetch_state_info(state_name, state_url):
                     if 'official language' in header_text.lower().replace('\xa0', ' '):
                         languages = row.find('td').get_text(strip=True)
                         if languages:
-                            matching_regex = re.match('[A-Z][a-z]*', languages)
+                            matching_regex = re.match('[A-Z][a-z]*( [A-Z][a-z]*)*', languages)
                             if matching_regex is not None:
                                 country_info['Language'] = matching_regex.group()
                         else:
@@ -141,30 +191,20 @@ def fetch_state_info(state_name, state_url):
                         continue
 
                     if 'time zone' in header_text.lower():
-                        time_zone = takewhile(lambda x: x != '(' and x != ' ', row.find('td').get_text(strip=True))
+                        time_zone = takewhile(lambda x: not is_character_parantheses(x) and x != ' ' and x != ';' and x != ',', row.find('td').get_text(strip=True))
                         country_info['Time zone'] = ''.join(time_zone)
                         continue
 
                     if header_text.lower() == 'government':
-                        political_regime = row.find('td').get_text(strip=False)
-                        country_info['Political regime'] = political_regime
+                        political_regime = takewhile(lambda x: not x.isdigit() and not is_character_parantheses(x), row.find('td').get_text(strip=False))
+                        country_info['Political regime'] = ''.join(political_regime)
 
     return State(country_info)
 
 
-def connect_to_database():
-    conn = psycopg2.connect(
-        dbname="states_of_the_world",
-        user="postgres",
-        password="parola",
-        host="localhost",
-        port=5432)
-    return conn
-
-
 def main():
-    conn = connect_to_database()
-    fetch_states(conn)
+  #  fetch_states(connect_to_database())
+    fetch_state_neighbours(connect_to_database())
 
 
 main()
