fd3bc72d44f0d4da1555a28f5d3b9de771a6f2e0
Savin Miruna <miruna.savin02@gmail.com>
2024-01-09 19:53:02 +0200

    add method documentation

--- Proiect/common.py
+++ Proiect/common.py
@@ -2,6 +2,14 @@ import psycopg2
 
 
 def connect_to_database():
+    """Establishes a connection to the 'states_of_the_world' database.
+
+    Returns:
+        psycopg2.extensions.connection: A connection to the 'states_of_the_world' database.
+
+    Raises:
+        psycopg2.OperationalError: If there's an issue in connecting to the database.
+    """
     conn = psycopg2.connect(
         dbname="states_of_the_world",
         user="postgres",
